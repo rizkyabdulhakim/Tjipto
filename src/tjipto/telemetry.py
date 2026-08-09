edc857c99196b97cbf9ab9bc1bbb8212e1ef1dac
rizky <rizkyabdulhakim.work@gmail.com>
2026-08-09 19:38:28 +0700

    Register the answer evaluation gate

--- src/tjipto/telemetry.py
+++ src/tjipto/telemetry.py
@@ -83,7 +83,7 @@ _INTEGRITY_REASONS = frozenset(
 )
 _CI_GATES = frozenset(
     {
-        "compileall", "unittest", "pytest", "pytest_run_1", "pytest_run_2", "retrieval_evaluation", "source_text_evaluation", "meaningful_support_evaluation", "support_reachability_evaluation", "artifact_validate", "artifact_rebuild", "ruff", "mypy",
+        "compileall", "unittest", "pytest", "pytest_run_1", "pytest_run_2", "retrieval_evaluation", "answer_evaluation", "source_text_evaluation", "meaningful_support_evaluation", "support_reachability_evaluation", "artifact_validate", "artifact_rebuild", "ruff", "mypy",
         "bandit", "pip_check", "pip_audit", "clean_tree", "release_validation", "web_test", "web_lint", "web_typecheck",
         "web_build", "web_smoke", "toolchain", "release_a", "release_b",
     }
